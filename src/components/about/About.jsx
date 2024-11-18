88739cad69d567b90b4817746d25092aaa3f9b79
proywanat <proywanat@gmail.com>
2024-11-18 23:07:48 +0700

    update: add aboutme

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import "./About.css";
+import Image from "../../assets/avatar-2.svg"; 
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
-  )
+    <section className="about container section" id="about">
+      <h2 className="section__title">About Me</h2>
+
+      <div className="about__container grid">
+        <img src={Image} alt="About Me" className="about__img" />
+
+        <div className="about__data grid">
+          <div className="about__info">
+            <p className="about__description">
+            Hi, I'm Wanat, a passionate Software Developer with a strong 
+            interest in Data Engineering. I have experience as a 
+            Full Stack Engineer and Software Developer, and I'm always eager
+            to learn and grow in both the data field and programming. 
+            Currently, I'm studying to enhance my data skills, with the goal
+            of transitioning into a Data Engineer role.
+            </p>
+            <a href="#download" className="btn">Download CV</a>
+          </div>
+        </div>
+
+        {/* <div className="about__skills grid">
+          <div className="skills__data">
+            <div className="skills__titles">
+              <h3 className="skills__name"></h3>
+              <span className="skills__number"></span>
+            </div>
+
+
+          </div>
+        </div> */}
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -3,25 +3,25 @@ import React from "react";
 const HeaderSocials = () => {
 
   const facebook__link = "https://www.facebook.com/proy.wanat";
-  const linkedin__link = "https://www.linkedin.com/in/wanatchaporn-churvivat";
+  const linkedin__link = "https://www.linkedin.com/in/wanatchaporn-churvivat-84bb24278";
   const github__link = "https://github.com/proywanat";
   const email__link = "mailto:proywanat@gmail.com";
 
   return (
     <div className="home__socials">
-      <a href={facebook__link} className="home__socials-link" target="_blank">
+      <a href={facebook__link} className="home__socials-link" target="_blank" rel="noreferrer">
         <i class="fa-brands fa-facebook"></i>
       </a>
 
-      <a href={linkedin__link} className="home__socials-link" target="_blank">
+      <a href={linkedin__link} className="home__socials-link" target="_blank" rel="noreferrer">
         <i class="fa-brands fa-linkedin"></i>
       </a>
 
-      <a href={github__link} className="home__socials-link" target="_blank">
+      <a href={github__link} className="home__socials-link" target="_blank" rel="noreferrer">
         <i class="fa-brands fa-github"></i>
       </a>
 
-      <a href={email__link} className="home__socials-link" target="_blank">
+      <a href={email__link} className="home__socials-link" target="_blank" rel="noreferrer">
         <i class="fa-regular fa-envelope"></i>
       </a>
     </div>
