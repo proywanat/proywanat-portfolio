8ec136d135be760dec7d0814507c0632a0685121
proywanat <proywanat@gmail.com>
2024-11-20 16:49:43 +0700

    update: fix aboutme and add css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import Image from "../../assets/avatar-2.svg"; 
+import Image from "../../assets/aboutme.png";
 
 const About = () => {
   return (
@@ -12,31 +12,36 @@ const About = () => {
 
         <div className="about__data grid">
           <div className="about__info">
+            <div className="about__details__exp">
+              <i className="icon-badge"></i>
+              <h3>Experience</h3>
+              <p>
+                1+ years <br /> Software Developer
+              </p>
+            </div>
+            <div className="about__details__skills">
+              <i className="icon-book-open"></i>
+              <h3>English Language Levels</h3>
+              <p>Elementary</p>
+            </div>
+          </div>
+          <div>
             <p className="about__description">
-            Hi, I'm Wanat, a passionate Software Developer with a strong 
-            interest in Data Engineering. I have experience as a 
-            Full Stack Engineer and Software Developer, and I'm always eager
-            to learn and grow in both the data field and programming. 
-            Currently, I'm studying to enhance my data skills, with the goal
-            of transitioning into a Data Engineer role.
+              Hi, I'm Wanat, a passionate Software Developer with a strong
+              interest in Data Engineering. I have experience as a Full Stack
+              Engineer and Software Developer, and I'm always eager to learn and
+              grow in both the data field and programming. Currently, I'm
+              studying to enhance my data skills, with the goal of transitioning
+              into a Data Engineer role.
             </p>
-            <a href="#download" className="btn">Download CV</a>
+            <a href="#download" className="btn">
+              Download CV
+            </a>
           </div>
         </div>
-
-        {/* <div className="about__skills grid">
-          <div className="skills__data">
-            <div className="skills__titles">
-              <h3 className="skills__name"></h3>
-              <span className="skills__number"></span>
-            </div>
-
-
-          </div>
-        </div> */}
       </div>
     </section>
   );
 }
 
-export default About;
+export default About
