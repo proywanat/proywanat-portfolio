adf4686128942eb398cd43e84512293ddf1bfe71
proywanat <proywanat@gmail.com>
2024-11-26 16:40:25 +0700

    update: add skill and exp card

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,38 @@
+const Data = [
+    {
+      id: 1,
+      category: "skills",
+      title: "Technical Skills",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+    {
+      id: 2,
+      category: "skills",
+      title: "Data Engineering Skills",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+    {
+      id: 3,
+      category: "skills",
+      title: "Other Skills",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+    {
+      id: 4,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "Dec 2023 - present",
+      title: "Software Developer | T-ECOSYS",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+    {
+      id: 5,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "Jul 2023 - Nov 2023",
+      title: "Full Stack Engineer | Jenosize",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+  ];
+
+export default Data
