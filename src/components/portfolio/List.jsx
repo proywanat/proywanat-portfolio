5275600355fd7264f9e5ae04991c6d23eebcd911
proywanat <proywanat@gmail.com>
2025-02-21 16:17:09 +0700

    update: add list

--- src/components/portfolio/List.jsx
+++ src/components/portfolio/List.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const List = ({list}) => {
+const List = ({ list, filterItems }) => {
+  const [active, setActive] = useState(0);
   return (
     <div className="portfolio__list">
-      {list.map((category, index) => {
-        return(
-          <button className="portfolio__list-item text-cs" key={index}>
+      {list
+        .filter((item) => item !== undefined)
+        .map((category, index) => (
+          <button
+            key={index}
+            className={`portfolio__list-item text-cs ${
+              active === index ? "active-work" : ""
+            }`}
+            onClick={() => {
+              setActive(index); 
+              filterItems(category);
+            }}
+          >
             {category}
           </button>
-        );
-      })}
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,11 +11,20 @@ const Portfolio = () => {
   const [projectItems, setMenuItems] = useState(Data);
   const [navList, setCategory] = useState(allNavList);
 
+  const filterItems = (category) => {
+    if(category === "All") { 
+      setMenuItems(Data);
+      return;
+    }
+    const newProjectItems = Data.filter((item) => item.category === category);
+    setMenuItems(newProjectItems);
+  }
+
   return (
     <section className="work container section" id="portfolio">
       <h2 className="section__title">Recent Works</h2>
 
-      <List list={navList} />
+      <List list={navList} filterItems={filterItems} />
 
       <div className="portfolio__container container grid">
         <Items projectItems={projectItems} />
